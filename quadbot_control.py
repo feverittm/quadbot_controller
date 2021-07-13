64c1a168ce12269ec13b3fe6480a505055135a94
Floyd Moore <floyd.moore@hp.com>
2021-07-13 08:10:29 -0700

    reformatting according to PEP standards

--- quadbot_control.py
+++ quadbot_control.py
@@ -1,40 +1,43 @@
 from tkinter import *
-from tkinter import messagebox
-import tkinter.font as tkFont
+import tkinter.font as tkfont
 
-def donothing():
-    x = 0
+
+def do_nothing():
+    pass
+
 
 def leg_buttons():
     lb_label = Label(master=legs_frame, text="Select Leg to Control:")
     lb_label.pack(side=TOP)
     lb_button = []
     for leg in range(4):
-        lb_button.append(Radiobutton(master=legs_frame, text=str(leg), \
-                value=str(leg), variable=selected_leg))
+        lb_button.append(Radiobutton(master=legs_frame, text=str(leg),
+                                     value=str(leg), variable=selected_leg))
         lb_button[leg].pack(side=LEFT, padx=15, fill=X)
+
 
 def build_sliders():
     # need 3 slider to control the position of each of the leg joints
-    sl_dict = {0 : "coxa", 1 : "femur", 2: "tibia"}
+    sl_dict = {0: "coxa", 1: "femur", 2: "tibia"}
     slider_label = []
     slider = []
     for item in range(3):
-        slider.append(Scale(master=slider_frame, from_=0, to=100, length=200
-            , variable=sl_dict[item]))
-        slider_label.append(Label(master=slider_frame, text = sl_dict[item]))
+        slider.append(Scale(master=slider_frame, from_=0, to=100, length=200,
+                            variable=sl_dict[item]))
+        slider_label.append(Label(master=slider_frame, text=sl_dict[item]))
         slider_label[item].pack(side=LEFT)
-        slider[item].pack(side=LEFT, padx = 10)
+        slider[item].pack(side=LEFT, padx=10)
+
 
 # need to create this as an array of 2x3 buttons and not just a linear list...
 def build_controls():
     controls_list = {
-            "reset":"cmd_reset",
-            "stand":"cmd_stand",
-            "forward":"cmd_fwd",
-            "back":"cmd_back",
-            "left":"cmd_left",
-            "right":"cmd_right"
+            "reset": "cmd_reset",
+            "stand": "cmd_stand",
+            "forward": "cmd_fwd",
+            "back": "cmd_back",
+            "left": "cmd_left",
+            "right": "cmd_right"
             }
     ctl_label = Label(master=controls_frame, text="Controls", font=fontStyle)
     ctl_label.pack()
@@ -43,38 +46,40 @@ def build_controls():
         ctl_button.append(Button(master=controls_frame, text=ckey, font=fontStyle))
         ctl_button[-1].pack(side=LEFT, padx=5, fill=X)
 
+
 def soft_buttons():
     sk_button = []
     for skey in range(5):
         text_string = "Soft Key " + str(skey)
-        sk_button.append(Button(master=sk_frame, text=text_string, \
-                command=lambda c=skey: onButtonClicked(c)))
+        sk_button.append(Button(master=sk_frame, text=text_string,
+                         command=lambda c=skey: onButtonClicked(c)))
         sk_button[skey].pack()
 
+
 def onButtonClicked(button_id):
-    print("Button" + str(button_id) + " is clicked!");
+    print("Button" + str(button_id) + " is clicked!")
+
 
 def build_menubar():
-    menubar = Menu(window, background="#ff8000", foreground='black'
-            , activebackground='white')
+    menubar = Menu(window, background="#ff8000", foreground='black',
+                   activebackground='white')
     filemenu = Menu(menubar, tearoff=0)
-    filemenu.add_command(label="New", command=donothing)
-    filemenu.add_command(label="Open", command=donothing)
-    filemenu.add_command(label="Save", command=donothing)
+    filemenu.add_command(label="New", command=do_nothing)
+    filemenu.add_command(label="Open", command=do_nothing)
+    filemenu.add_command(label="Save", command=do_nothing)
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command=window.quit)
     menubar.add_cascade(label="File", menu=filemenu)
 
     helpmenu = Menu(menubar, tearoff=0)
-    helpmenu.add_command(label="Help Index", command=donothing)
-    helpmenu.add_command(label="About...", command=donothing)
+    helpmenu.add_command(label="Help Index", command=do_nothing)
+    helpmenu.add_command(label="About...", command=do_nothing)
     menubar.add_cascade(label="Help", menu=helpmenu)
 
     window.config(menu=menubar)
 
 # ------ main ------
 
-selected_leg=0
 
 # top level window stuff...
 window = Tk()
@@ -82,7 +87,7 @@ window.title("Quadbot Controls")
 window.geometry("350x350")
 build_menubar()
 
-fontStyle = tkFont.Font(family="Lucida Grande", size=18)
+fontStyle = tkfont.Font(family="Lucida Grande", size=18)
 
 selected_leg = StringVar(window, "0")
 
@@ -94,7 +99,7 @@ selected_leg = StringVar(window, "0")
 #       left frame is split in two vertically to hold the leg selection on top
 #       and the sliders below.  
 #
-#The size of the slider should drive the size of the main window.
+# The size of the slider should drive the size of the main window.
 #
 # -- top frame has 3 sections...
 top_frame = Frame()
@@ -118,7 +123,7 @@ bot_frame = Frame()
 
 controls_frame = Frame(master=bot_frame)
 build_controls()
-controls_frame.pack(side=BOTTOM, expand = True, fill=X)
+controls_frame.pack(side=BOTTOM, expand=True, fill=X)
 
 bot_frame.pack(side=BOTTOM, fill=X)
 
